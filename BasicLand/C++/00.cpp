ae1de0534c8e3c67b86751123ea51da7bf1dc779
Tknott95 <tknott95@hotmail.com>
2020-05-10 23:25:30 -0600

    Satic dotProd() + bias, rm cout's | C++ 00.cpp

--- BasicLand/C++/00.cpp
+++ BasicLand/C++/00.cpp
@@ -26,11 +26,6 @@ int main() {
     {i[0]*w[0][0] + i[1]*w[0][1] + i[2]*w[0][2] + i[3]*w[0][3] + b[0]},
     {i[0]*w[1][0] + i[1]*w[1][1] + i[2]*w[1][2] + i[3]*w[1][3] + b[1]}
   };
- 
-  cout << i[0] << endl;
-  cout << w[0][0] << endl;
-  cout << w[1][0] << endl;
-  cout << i[0]*w[0][0] << endl;
 
   cout << output[0][0] << endl;
   cout << output[1][0] << endl;
