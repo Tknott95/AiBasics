5b8cb0d0843150f5ff44af6d54ff57e3f1534954
Tknott95 <tknott95@hotmail.com>
2020-05-17 10:42:16 -0600

    ObjOrient Tf2 and Keras | objOritentTf2/00.py

---
+++ Tf2KerasRef/ObjOrientTf2/00.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+# Using mnist, the "HelloWorld dataset of AI"
+mnist = tf.keras.datasets.mnist
+
+# Declaring my vars to hold data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Add a channels dimension. @TODO look into this, adding all dims prior via. ... then adding the new dim
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+# Making a model class for ObjOrient
+# Built w/ keras subclassing API
+class ModelClass(Model):
+  def __init__(self):
+    super(ModelClass, self).__init__()
+    self.conv01 = Conv2D(32, 3, activation='relu')
+    self.flatten = Flatten()
+    self.d01 = Dense(128, activation='relu')
+    self.d02 = Dense(10)
+  def call(self, x):
+    x = self.conv01(x)
+    x = self.flatten(x)
+    x = self.d01(x)
+    return self.d02(x)
+
+model = ModelClass()
+
+# Choosing my optimizer homie quan
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam()
+
+# Selecting metrics to to measure loss and accuracy of the model. Metrics accumulate the vals over epochs and prints overall result
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+# Using "tf.GradientTape" to train the model. @TODO look into this
+@tf.function
+def train_step(images, labels):
+  with tf.GradientTape() as tape:
+    # training=True is only needed if there are layers with different behavior during training versus inference (e.g. Dropout)
+    predictions = model(images, training=True)
+    loss = loss_object(labels, predictions)
+  gradients = tape.gradient(loss, model.trainable_variables) # @TODO look intomodel.trainable_variables 
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  train_loss(loss)
+  train_accuracy(labels, predictions)
+
+@tf.function
+def test_step(images, objects):
+  # training=False is only needed if there are layers with different behavior during training versus inference (e.g. Dropout)
+  predictions = model(images, training=False)
+  t_loss = loss_object(labels, predictions)
+
+  test_loss(t_loss)
+  test_accuracy(labels, predictions)
+
+EPOCHS = 5
+for epoch in range(EPOCHS):
+  # Reset the metrics at the start of the next epoch
+  train_loss.reset_states()
+  train_accuracy.reset_states()
+  test_loss.reset_states()
+  test_accuracy.reset_states()
+
+  for images, labels in train_dataset:
+    train_step(images, labels)
+  for test_images, test_labels in test_dataset:
+    test_step(test_images, test_labels)
+  
+  template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+  print(template.format(epoch+1,
+                        train_loss.result(),
+                        train_accuracy.result()*100,
+                        test_loss.result(),
+                        test_accuracy.result()*100))
