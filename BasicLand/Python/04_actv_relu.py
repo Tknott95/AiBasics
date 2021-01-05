42d9afd6e11d5bec07941e3a94b6204e843e6f2e
Trevor Knott <Tknott95@hotmail.com>
2021-01-05 07:29:14 -0700

    rnmng 0(3|4).py

--- BasicLand/Python/04_actv_relu.py
+++ BasicLand/Python/04_actv_relu.py
@@ -2,52 +2,13 @@ import numpy as np
 
 """
   ACTIVATION FUNCTION 00 - ReLU
+
+  - only applying to the first layer for an ouptu comparison for visuals.
+  - will not be passing the output after an activation function pass into layer 2. 
+  - complexity right meow is not needed rather visuals for learning each "module"
 """
  
 np.random.seed(4)
-
-"""
-|| Creating an obj w/ random generators for neural net theory ||
-  i -> inputs   | when array, they become batches
-  w -> weights  | will be random for this example
-  b -> bias     | 
-
-@NOTES
-  dot prod w/ numpy             -> np.dot()
-  random seed w/ same rand vals -> np.random.seed(int)
-
-  np.random.randn()              -> randn generates an array from the shape given of rand floats from
-    a "univariate normal Gaussian distribution" | a rand float generator for arrays essentially from -1 <-> 1
-    using 0.10 * np.random.randn() for better "mock data"
-
-    Example: 
-      run    -> print(np.random.randn(2,3))
-      output -> [[0.79,-0.2, 0.3], [-0.1,-0.42,0.44]]
-
-      run    -> print(np.random.randn(4,3))
-      output -> [
-        [ 0.05056171  0.49995133 -0.99590893]
-        [ 0.69359851 -0.41830152 -1.58457724]
-        [-0.64770677  0.59857517  0.33225003]
-        [-1.14747663  0.61866969 -0.08798693]]
-
-  np.zeros()                     -> returns a tuple of an array of the shape
-    Example:
-      run    -> print(np.zeros(4))
-      output -> [0. 0. 0. 0.]
-
-      run    -> print(np.zeros((1, 4)))
-      output -> [[0. 0. 0. 0.]]
-
-      run    -> print(np.zeros((2, 4)))
-      output -> [
-        [0. 0. 0. 0.]
-        [0. 0. 0. 0.]]
-
-  @Custom
-    Layer_Dense(<numberOfInputs/features, so 4>, <num of neurons, can be any #>)
-     next layer input = lastLayer.output
-"""
 
 def main():
   i = [
