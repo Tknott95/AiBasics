564a16e2127d4bf451780055ad9d434e2d44c438
Trevor Knott <Tknott95@hotmail.com>
2021-01-09 07:54:52 -0700

    CategoricalCrossEntropyNoLayers DONE | 07_categorical_cross_entropy_no_layers.py

--- BasicLand/Python/07_categorical_cross_entropy_no_layers.py
+++ BasicLand/Python/07_categorical_cross_entropy_no_layers.py
@@ -1,7 +1,14 @@
 import numpy as np
 
-netOutput = [0.88, 0.24, 0.14]
-targetOutput = [1, 0, 0]
+# As said in the read me this code is very heavily nnfs book w/ twkz to learn n.n.f.s
+netOutputs = np.array([
+  [0.88, 0.12, 0.2],
+  [0.1, 0.76, 0.41],
+  [0.02, 0.84, 0.08]])
+targetOutputs = np.array([
+  [1, 0, 0],
+  [0, 1, 0],
+  [0, 1, 0]])
 
 class Loss:
   def calculate(self, output, y):
@@ -9,4 +16,29 @@ class Loss:
     dataLoss = np.mean(sampleLosses)
 
     return dataLoss
-  
+
+class CategoricalCrossEntropyLoss(Loss):
+  def forward(self, yPrediction, yTrue):
+    numOfSamples = len(yPrediction)
+    """ NNFS note on line/code below
+      Clip data to prevent division by 0. 
+      Clip both sides to not drag mean towards any value """
+    yPredictionClipped = np.clip(yPrediction, 1e-7, 1 - 1e-7)
+
+    if len(yTrue.shape) == 1:
+      correctConfidences = yPredictionClipped[
+        range(numOfSamples),
+        True
+      ]
+    elif len(yTrue.shape) == 2:
+      correctConfidences = np.sum(
+        yPredictionClipped*yTrue,
+        axis=1
+      )
+    
+    negativeLogLikelihoods = -np.log(correctConfidences)
+    return negativeLogLikelihoods
+
+lossFunction = CategoricalCrossEntropyLoss()
+lossVal = lossFunction.calculate(netOutputs, targetOutputs)
+print("  Loss:  ", lossVal)
