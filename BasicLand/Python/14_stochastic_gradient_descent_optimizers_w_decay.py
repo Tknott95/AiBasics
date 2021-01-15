7e54fa4d92cddebd36e092902efa662391d348ac
Trevor Knott <Tknott95@hotmail.com>
2021-01-15 07:37:19 +0000

    Decay smaller from 1e-2 -> 1e-3 | 14_stochastic_gradient

--- BasicLand/Python/14_stochastic_gradient_descent_optimizers_w_decay.py
+++ BasicLand/Python/14_stochastic_gradient_descent_optimizers_w_decay.py
@@ -126,7 +126,7 @@ class Main:
   layer2 = LayerDense(64,3)
   lossActivation = ActivationSoftmaxLossCategoricalCrossEntropy()
 
-  optimizer = OptimizerSGD(decay=1e-2)
+  optimizer = OptimizerSGD(decay=1e-3)
  
   for epoch in range(10044):
     layer1.forward(X)
