d589a908162f9e9c37497189474437ccf3554118
Tknott95 <tknott95@hotmail.com>
2021-04-22 22:28:21 -0600

    createMnistData() DONE() | 24_net_w_data_00.py

--- BasicLand/Python/24_net_w_data_00.py
+++ BasicLand/Python/24_net_w_data_00.py
@@ -29,7 +29,6 @@ def createMnistData(path):
   return X, y, xTest, yTest
 
 
-# @NOTE Leaving out validation data
 class LayerDense:
     def __init__(self, _numOfInputs, _numOfNeurons, weightRegularizerL1=0, weightRegularizerL2=0, biasRegularizerL1=0, biasRegularizerL2=0):
       self.weights = 0.1 * np.random.randn(_numOfInputs, _numOfNeurons)
