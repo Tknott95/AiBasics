a6851e289899bf3ba025ae510847b9eab7d10486
Tknott95 <tknott95@hotmail.com>
2021-04-22 22:11:11 -0600

    Data fetching script 4 nnfs MNIST .zip |  u24_fetching_nnfs_assets.py

---
+++ BasicLand/Python/u24_fetching_nnfs_assets.py
@@ -0,0 +1,10 @@
+import os
+"""
+  - SCRIPT PULLS NNFS .zip DATA. Using book conventions.
+  Libs for pulling my zip and decompressing it
+"""
+DATA_URL = 'https://nnfs.io/datasets/fashion_mnist_images.zip'
+DATA_FILE = 'fashion_mnist_images.zip'
+DATA_FOLDER = 'fashion_mnist_images'
+
+
